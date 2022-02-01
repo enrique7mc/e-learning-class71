ae16dd5084d7ff49ca21362506a91e32e5986a4f
Enrique Munguía <xemc@pm.me>
2022-02-01 15:30:16 -0800

    Use firebase v8

--- config.js
+++ config.js
@@ -1,16 +1,18 @@
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyD185bhmHeEC9bpOfOZewCpXfyOn3UqwUg",
   authDomain: "bibliotecadigitalprueba-d62e9.firebaseapp.com",
   projectId: "bibliotecadigitalprueba-d62e9",
   storageBucket: "bibliotecadigitalprueba-d62e9.appspot.com",
   messagingSenderId: "106503193550",
   appId: "1:106503193550:web:38affd2a0258c412b2619f",
-};
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+});
+const db = firebase.firestore();
+
+export default db;
